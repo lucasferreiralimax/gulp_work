a07678bf9d39649e307e33387353b50796a861ed
Lucas Ferreira Lima <lucasferreiralimax@gmail.com>
2019-09-24 00:28:42 -0300

    organizando em ordem alfabética :hammer:

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,31 @@
 const { src, dest, series, watch, parallel } = require('gulp');
-const minifyJS = require('gulp-uglify-es').default;
-const stylus = require('gulp-stylus');
-const pug = require('gulp-pug');
 const babel = require('gulp-babel');
-const inject = require('gulp-inject');
 const connect = require('gulp-connect');
 const del = require('del');
+const inject = require('gulp-inject');
+const minifyJS = require('gulp-uglify-es').default;
+const pug = require('gulp-pug');
+const stylus = require('gulp-stylus');
 
 function clean_work() {
   return del('dist/**/*', {force:true})
+}
+
+function connect_dist(done) {
+  connect.server({
+    name: 'Dist',
+    root: 'dist',
+    port: 8000,
+    livereload: true
+  })
+  done()
+}
+
+function css() {
+  return src(['src/**/*.styl', '!src/**/[_]*.styl'])
+    .pipe(stylus())
+    .pipe(dest('dist/'))
+    .pipe(connect.reload())
 }
 
 function html() {
@@ -24,13 +41,6 @@ function html() {
     .pipe(connect.reload())
 }
 
-function css() {
-  return src(['src/**/*.styl', '!src/**/[_]*.styl'])
-    .pipe(stylus())
-    .pipe(dest('dist/'))
-    .pipe(connect.reload())
-}
-
 function js() {
   return src('src/**/*.js', { sourcemaps: false })
     .pipe(babel({
@@ -40,25 +50,16 @@ function js() {
     .pipe(connect.reload())
 }
 
-function connect_dist(done) {
-  connect.server({
-    name: 'Dist',
-    root: 'dist',
-    port: 8000,
-    livereload: true
-  })
-  done()
-}
-
 function watchDev(done) {
   watch(['src/**/*.styl', 'src/**/*.pug', 'src/**/*.js'], series(clean_work, css, js, html))
   done()
 }
 
-exports.js = js;
-exports.css = css;
-exports.html = html;
 exports.clean_work = clean_work;
 exports.connect_dist = connect_dist;
+exports.css = css;
+exports.html = html;
+exports.js = js;
 exports.watchDev = watchDev;
-exports.default = series(clean_work, css, js, html, connect_dist, watchDev);
+
+exports.default = series(clean_work, css, html, js, connect_dist, watchDev);
